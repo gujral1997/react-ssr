24b9e441d28309491954949adc809bb03bc9b770
Ansh Gujral <anshgujral1997@gmail.com>
2020-05-24 03:14:33 +0530

    setup proxy
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,14 +4,22 @@ import React from "react"
 import { hydrate } from "react-dom"
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
+import axios from "axios"
 import Routes from './Routes'
 import reducers from "./reducers"
 import { applyMiddleware, createStore } from "redux"
 import thunk from "redux-thunk"
 import { renderRoutes } from "react-router-config"
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk))
+const axiosInstance = axios.create({
+    baseURL: '/api'
+})
+
+const store = createStore(
+    reducers,
+    window.INITIAL_STATE,
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
+)
 
 hydrate(
     <Provider store={store}>
